b98b68f9a0b491094566b6b76d8344f9fd73f3a2
Garrett Morris <gmorris_89@outlook.com>
2019-08-09 23:20:58 -0500

    my fork of node-logger

--- logger.js
+++ logger.js
@@ -1,10 +1,9 @@
 // name: logger.js
 // version: 0.0.1
 // http://github.com/quirkey/node-logger
+// forked by Garrett Morris
 /*
-
-Copyright (c) 2010 Aaron Quint
-
+Copyright (c) 2010 Aaron Quint, 2019 Garrett Morris
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without
@@ -13,10 +12,8 @@ copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following
 conditions:
-
 The above copyright notice and this permission notice shall be
 included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -25,91 +22,97 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-
 */
 
-var path = require('path'),
-    sys  = require('sys'),
-    fs   = require('fs');
+const colors = require('colors/safe');
+const util = require('util');
 
-var makeArray = function(nonarray) { 
-  return Array.prototype.slice.call(nonarray); 
-};
+function isFormattableString( args ) {
+    // console.log(args);
+    if(!args.length) return false;
+    if(typeof args[0] !== 'string') return false;
+    if( args[0].includes('%s') || args[0].includes('%d') || args[0].includes('%i') || args[0].includes('%f') || args[0].includes('%j') || args[0].includes('%o') || args[0].includes('%O') ) {
+        return true;
+    }
+    return false;
+}
 
-// Create a new instance of Logger, logging to the file at `log_file_path`
-// if `log_file_path` is null, log to STDOUT.
-var Logger = function(log_file_path) {
-  // default write is STDOUT
-  this.write     = sys.print;
-  this.log_level_index = 3;
-  
-  // if a path is given, try to write to it
-  if (log_file_path) {
-    // Write to a file
-    log_file_path = path.normalize(log_file_path);
-    this.stream = fs.createWriteStream(log_file_path, {flags: 'a', encoding: 'utf8', mode: 0666});
-    this.stream.write("\n");
-    this.write = function(text) { this.stream.write(text); };
-  }
-};
+class Logger {
 
-Logger.levels = ['fatal', 'error', 'warn', 'info', 'debug'];
+    constructor(log_level='debug',transports=[console.log]) {
 
-// The default log formatting function. The default format looks something like:
-//
-//    error [Sat Jun 12 2010 01:12:05 GMT-0400 (EDT)] message
-// 
-Logger.prototype.format = function(level, date, message) {
-  return [level, ' [', date, '] ', message].join('');
-};
+        this.transports = transports; // default write is STDOUT
+        this.levels = ['emerg','alert','crit','error','warning','notice','info','debug'];
+        this.colors = ['red','red','red','red','yellow','cyan','cyan','green']; //colors match up to levels
+        this.setLevel(log_level);
 
-// Set the maximum log level. The default level is "info".
-Logger.prototype.setLevel = function(new_level) {
-  var index = Logger.levels.indexOf(new_level);
-  return (index != -1) ? this.log_level_index = index : false;
-};
+    }
 
-// The base logging method. If the first argument is one of the levels, it logs
-// to that level, otherwise, logs to the default level. Can take `n` arguments
-// and joins them by ' '. If the argument is not a string, it runs `sys.inspect()`
-// to print a string representation of the object.
-Logger.prototype.log = function() {
-  var args = makeArray(arguments),
-      log_index = Logger.levels.indexOf(args[0]),
-      message = '';
+    setLevel(new_level) {
+        var index = this.levels.indexOf(new_level);
+        return (index != -1) ? this.log_level_index = index : false;
+    }
 
-  // if you're just default logging
-  if (log_index === -1) { 
-    log_index = this.log_level_index; 
-  } else {
-    // the first arguement actually was the log level
-    args.shift();
-  }
-  if (log_index <= this.log_level_index) {
-    // join the arguments into a loggable string
-    args.forEach(function(arg) {
-      if (typeof arg === 'string') {
-        message += ' ' + arg;
-      } else {
-        message += ' ' + sys.inspect(arg, false, null);
-      }
-    });
-    message = this.format(Logger.levels[log_index], new Date(), message);
-    this.write(message + "\n");
-    return message;
-  }
-  return false;
-};
+    write(text) {
+        this.transports.forEach(transport=>transport(text));
+    }
 
-Logger.levels.forEach(function(level) {
-  Logger.prototype[level] = function() {
-    var args = makeArray(arguments);
-    args.unshift(level);
-    return this.log.apply(this, args);
-  };
-});
+    format(level, date, message) {
+        // console.log(level);
+        return [ colors[ this.colors[this.levels.indexOf(level)] ](level), ' [', date, '] ', message].join('');
+    }
 
-exports.Logger = Logger;
-exports.createLogger = function(log_file_path) {
-  return new Logger(log_file_path);
-};
+    log(...args) {
+        // console.log(Array.isArray(args));
+        if(!args.length){
+            return;
+        }
+        const log_index = this.levels.indexOf(args[0]);
+        let message = '';
+
+        // if you're just default logging
+        if (log_index === -1) { 
+            log_index = 7; 
+        } else {
+            // the first argument actually was the log level
+            args.shift();
+        }
+        if (log_index <= this.log_level_index) {
+            if( isFormattableString(args) ) {
+                // console.log('logger: formatting message');
+                message = util.format(...args);
+            }else{
+                // join the arguments into a loggable string
+                args.forEach(arg=> {
+                    if (typeof arg === 'string') {
+                        message += ' ' + arg;
+                    } else {
+                        message += ' ' + util.inspect(arg, false, null);
+                    }
+                });
+            }
+            
+            message = this.format(this.levels[log_index], new Date(), message);
+            this.write(message + "\n");
+            return message;
+        }
+        return false;
+    }
+
+}
+
+function createLogger(...args) {
+    const logger = new Logger(...args);
+    const log = (...args) => {
+        logger.log('info',...args);
+    };
+    log.logger = logger;
+
+    logger.levels.forEach(level=>{
+        log[level] = (...args) => logger.log(level,...args);
+    })  
+    return log;  
+}
+
+
+module.exports = createLogger;
