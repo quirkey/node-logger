2108a56d0c6233c29357197ca03b610cc7e96b5f
onblog <codeblog@qq.com>
2020-06-05 17:39:40 +0800

    update

--- logger.js
+++ logger.js
@@ -29,69 +29,94 @@ OTHER DEALINGS IN THE SOFTWARE.
 */
 
 var path = require('path'),
-    sys  = require('sys'),
-    fs   = require('fs');
+    util = require('util'),
+    fs = require('fs');
 
-var makeArray = function(nonarray) { 
-  return Array.prototype.slice.call(nonarray); 
+var makeArray = function (nonarray) {
+  return Array.prototype.slice.call(nonarray);
 };
 
 // Create a new instance of Logger, logging to the file at `log_file_path`
 // if `log_file_path` is null, log to STDOUT.
-var Logger = function(log_file_path) {
+var Logger = function (log_file_path) {
+  this.default_log_level_index = 3;
+
   // default write is STDOUT
-  this.write     = sys.print;
-  this.log_level_index = 3;
-  
+  this.log_level_index = 0;
+  this.write = function (text) {
+    text =  text.trim()
+    switch (this.log_level_index) {
+      case 0:
+        console.log(text)
+        break
+      case 1:
+        console.error(text)
+        break
+      case 2:
+        console.warn(text)
+        break
+      case 3:
+        console.info(text)
+        break
+      case 4:
+        console.debug(text)
+        break
+    }
+  };
+
   // if a path is given, try to write to it
   if (log_file_path) {
     // Write to a file
     log_file_path = path.normalize(log_file_path);
-    this.stream = fs.createWriteStream(log_file_path, {flags: 'a', encoding: 'utf8', mode: 0666});
-    this.stream.write("\n");
-    this.write = function(text) { this.stream.write(text); };
+    this.stream =
+        fs.createWriteStream(log_file_path, {flags: 'a', encoding: 'utf8', mode: 0o666});
+    this.write = function (text) {
+      this.stream.write(text);
+    };
   }
 };
 
-Logger.levels = ['fatal', 'error', 'warn', 'info', 'debug'];
+Logger.levels = ['trace', 'error', 'warn', 'info', 'debug'];
+
 
 // The default log formatting function. The default format looks something like:
 //
 //    error [Sat Jun 12 2010 01:12:05 GMT-0400 (EDT)] message
-// 
-Logger.prototype.format = function(level, date, message) {
-  return [level, ' [', date, '] ', message].join('');
+//
+Logger.prototype.format = function (level, date, message) {
+  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getMinutes()} [${level.toUpperCase()}] ${message}`;
 };
 
 // Set the maximum log level. The default level is "info".
-Logger.prototype.setLevel = function(new_level) {
-  var index = Logger.levels.indexOf(new_level);
-  return (index != -1) ? this.log_level_index = index : false;
+Logger.prototype.setLevel = function (new_level) {
+  var index = Logger.levels.indexOf(new_level.toLowerCase());
+  return (index !== -1) ? this.default_log_level_index = index : false;
 };
 
 // The base logging method. If the first argument is one of the levels, it logs
 // to that level, otherwise, logs to the default level. Can take `n` arguments
 // and joins them by ' '. If the argument is not a string, it runs `sys.inspect()`
 // to print a string representation of the object.
-Logger.prototype.log = function() {
+Logger.prototype.log = function () {
   var args = makeArray(arguments),
       log_index = Logger.levels.indexOf(args[0]),
       message = '';
 
   // if you're just default logging
-  if (log_index === -1) { 
-    log_index = this.log_level_index; 
+  if (log_index === -1) {
+    log_index = this.default_log_level_index;
   } else {
     // the first arguement actually was the log level
     args.shift();
   }
-  if (log_index <= this.log_level_index) {
+  if (log_index < Logger.levels.length) {
+    this.log_level_index = log_index
     // join the arguments into a loggable string
-    args.forEach(function(arg) {
+    args.forEach(function (arg) {
       if (typeof arg === 'string') {
         message += ' ' + arg;
       } else {
-        message += ' ' + sys.inspect(arg, false, null);
+        message += ' ' + util.inspect(arg, false, null);
       }
     });
     message = this.format(Logger.levels[log_index], new Date(), message);
@@ -101,8 +126,8 @@ Logger.prototype.log = function() {
   return false;
 };
 
-Logger.levels.forEach(function(level) {
-  Logger.prototype[level] = function() {
+Logger.levels.forEach(function (level) {
+  Logger.prototype[level] = function () {
     var args = makeArray(arguments);
     args.unshift(level);
     return this.log.apply(this, args);
@@ -110,6 +135,6 @@ Logger.levels.forEach(function(level) {
 });
 
 exports.Logger = Logger;
-exports.createLogger = function(log_file_path) {
+exports.createLogger = function (log_file_path) {
   return new Logger(log_file_path);
 };
